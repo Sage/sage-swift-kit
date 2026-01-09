447de6eefdcc2d945153af1815fe9068257cb458
Tiago Pereira <tiago.fpereira@sage.com>
2026-01-09 12:29:39 +0100

    Update/swift syntax
    update-swift-syntax

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0")
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "604.0.0-prerelease-2025-12-15")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
